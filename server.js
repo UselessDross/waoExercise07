8ee9b24f6d029c35c7118c99dfb7f465db8a6e1c
Useless-4every1 <104831784+UselessDross@users.noreply.github.com>
2024-02-25 13:51:14 +0100

    npm init -y npm install apollo-server

---
+++ server.js
@@ -0,0 +1,25 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTcwODg2NTAzNywiZXhwIjoxNzA5NDY5ODM3fQ.rtDWo328r54m4-YO0wQsoWAuSCcHsT-ovNvB1cwgti0"
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
